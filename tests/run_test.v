40016e7440af1d1b6090cabab9f308010ef6a095
Felipe Pena <felipensp@gmail.com>
2024-03-13 09:28:59 -0300

    test

--- tests/run_test.v
+++ tests/run_test.v
@@ -1,7 +1,16 @@
 import os
 
-fn test_helloworld() {
+fn test_run_helloworld() {
 	res := os.execute('./vtcc -Iinclude -run hello.c')
 	assert res.exit_code == 0
 	assert res.output.trim_space() == 'hello world!'
+}
+
+fn test_build_helloworld() {
+	res := os.execute('./vtcc -Iinclude -o ./hello hello.c')
+	assert res.exit_code == 0
+
+	res2 := os.execute('./hello')
+	os.rm('./hello')!
+	assert res2.output.trim_space() == 'hello world!'
 }
